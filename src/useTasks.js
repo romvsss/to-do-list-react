edf12bbac73765aa7835a6afd133f0a9e3c7f2ce
Oskar Romanowicz <oskar.romanowicz@onet.pl>
2025-10-19 18:27:01 +0200

    useTasks hook added

---
+++ src/useTasks.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+
+const defaultTasks = [];
+
+export const useTasks = () => {
+    const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem('tasks') || defaultTasks));
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
+
+    const removeTask = (id) => {
+        setTasks(tasks => tasks.filter(task => task.id !== id));
+    };
+
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, done: !task.done };
+            }
+            return task;
+        }));
+    };
+
+    const setAllDone = () => {
+        setTasks(tasks => tasks.map(task => ({ ...task, done: true })));
+    };
+
+    const addNewTask = (newTaskContent) => {
+        setTasks(tasks => [
+            ...tasks,
+            {
+                content: newTaskContent,
+                done: false,
+                id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
+            }
+        ]);
+    }
+    return {
+        tasks,
+        removeTask,
+        toggleTaskDone,
+        setAllDone,
+        addNewTask,
+    };
+}
